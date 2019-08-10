c09aad9dc8b1f8964b15ef8cb48ff50321388bdf
flyduckforever <flyduckforever@gmail.com>
2019-08-10 16:06:07 +0800

    整合PageHelper分页功能

--- src/main/java/com/biz/web/MyController.java
+++ src/main/java/com/biz/web/MyController.java
@@ -2,10 +2,13 @@ package com.biz.web;
 
 import com.biz.pojo.Hero;
 import com.biz.service.HeroService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,8 +22,17 @@ public class MyController {
     private HeroService heroService;
     @RequestMapping("getAllHero")
     public String getAllHero(){
+        PageHelper.startPage(2,4);
         List<Hero> allHero = heroService.getAllHero();
-        System.out.println(allHero);
+        PageInfo pageInfo = new PageInfo(allHero,3);
+        List<Hero> list = pageInfo.getList();
+        for (Hero o : list) {
+            System.out.println(o);
+        }
+        System.out.println("当前页："+pageInfo.getPageNum());
+        System.out.println("总页数: "+pageInfo.getPages());
+        System.out.println("导航："+ Arrays.toString(pageInfo.getNavigatepageNums()));
+        System.out.println("总数："+pageInfo.getTotal());
         return "hello";
     }
 }
